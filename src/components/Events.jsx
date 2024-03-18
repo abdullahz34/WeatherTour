6a843e925896225ff4c029e34868e8736611959b
Sahra <ec22951@qmul.ac.uk>
2024-03-18 21:56:05 +0000

    fixing css grid

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,7 +4,18 @@ import './Events.css';
 const Events = ({ data }) => {
     return (
         <div className="events">
-            
+            <h2 className='title'>
+                Events near you
+            </h2>
+            <div className='placeholder-content'>
+                <div className='placeholder-image'>
+                    <img src='icons/01d.png' />
+                </div>
+                <div className='placeholder-text'>
+                    <h3>The Shard</h3>
+                    <p>Outdoor activity that's suited for more sunny weather</p>
+                </div>
+            </div>
         </div>
     );
 };
