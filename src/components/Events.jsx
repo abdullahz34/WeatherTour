55b5b78377af304539d10d6a116647c5f645c3b9
Sahra <ec22951@qmul.ac.uk>
2024-03-20 20:32:43 +0000

    added another event

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -8,12 +8,23 @@ const Events = ({ data }) => {
                 Events near you
             </h2>
             <div className='placeholder-content'>
-                <div className='placeholder-image'>
-                <img src="/icons/01d.png" alt="" />
+                <div className='placeholder-content1'>
+                    <div className='placeholder-image'>
+                        <img src="/icons/01d.png" alt="" />
+                    </div>
+                    <div className='placeholder-text'>
+                        <h3>The Shard</h3>
+                        <p>Outdoor activity that's suited for more sunny weather</p>
+                    </div>
                 </div>
-                <div className='placeholder-text'>
-                    <h3>The Shard</h3>
-                    <p>Outdoor activity that's suited for more sunny weather</p>
+                <div className='placeholder-content2'>
+                    <div className='placeholder-image2'>
+                        <img src="/icons/03d.png" alt="" />
+                    </div>
+                    <div className='placeholder-text2'>
+                        <h3>Arcade</h3>
+                        <p>Indoor activity that's suited for more cloudy weather</p>
+                    </div>
                 </div>
             </div>
         </div>
