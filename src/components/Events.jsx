4c164208c93ec62c0ba4bfba589a61faf979cf99
abdullahz34 <usamash34@gmail.com>
2024-03-21 19:45:34 +0000

    feat: added updating tags and links for titles poi

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -12,20 +12,28 @@ const Events = ({ poiData1, poiData2, poiData }) => {
             <div className='placeholder-content'>
                 <div className='placeholder-content1'>
                     <div className='placeholder-image'>
-                    <img src={poiData1 && poiData1.weather && poiData1.weather[0] ? `icons/${poiData1.weather[0].icon}.png` : 'default_icon.png'} alt="weather" />
+                        <img src={poiData1 && poiData1.weather && poiData1.weather[0] ? `icons/${poiData1.weather[0].icon}.png` : 'default_icon.png'} alt="weather" />
                     </div>
                     <div className='placeholder-text'>
-                        <h3>{poiData.data[0].name}</h3>
-                        <p>Outdoor activity that's suited for more sunny weather</p>
+                        <h3>
+                            <a href={`https://www.google.com/search?q=${encodeURIComponent(poiData.data[0].name)}`} target="_blank" rel="noopener noreferrer">
+                                {poiData.data[0].name}
+                            </a>
+                        </h3>
+                        <p>Tags: {poiData && poiData.data && poiData.data[0] && poiData.data[0].tags ? poiData.data[0].tags.slice(0, 5).map((tag, index) => <span key={index}>{tag}{index < 4 ? ', ' : ''}</span>) : 'Loading...'}</p>
                     </div>
                 </div>
                 <div className='placeholder-content2'>
                     <div className='placeholder-image2'>
-                    <img src={poiData2 && poiData2.weather && poiData2.weather[0] ? `icons/${poiData2.weather[0].icon}.png` : 'default_icon.png'} alt="weather" />
+                        <img src={poiData2 && poiData2.weather && poiData2.weather[0] ? `icons/${poiData2.weather[0].icon}.png` : 'default_icon.png'} alt="weather" />
                     </div>
                     <div className='placeholder-text2'>
-                        <h3>{poiData.data[1].name}</h3>
-                        <p>Indoor activity that's suited for more cloudy weather</p>
+                        <h3>
+                            <a href={`https://www.google.com/search?q=${encodeURIComponent(poiData.data[1].name)}`} target="_blank" rel="noopener noreferrer">
+                                {poiData.data[1].name}
+                            </a>
+                        </h3>
+                        <p>Tags: {poiData && poiData.data && poiData.data[1] && poiData.data[1].tags ? poiData.data[1].tags.slice(0, 5).map((tag, index) => <span key={index}>{tag}{index < 4 ? ', ' : ''}</span>) : 'Loading...'}</p>
                     </div>
                 </div>
             </div>
